bdedf5e76bc42ac04d1d3847b9664aef3462cdf0
Julian Salazar <hjsalazar9@gmail.com>
2025-04-06 22:32:22 -0500

    Add CatBreed entity model

---
+++ challenge_pragma_ios/challenge_pragma_ios/Domain/Entities/CatBreed.swift
@@ -0,0 +1,16 @@
+//
+//  CatBreed.swift
+//  challenge_pragma_ios
+//
+//  Created by Mac on 6/04/25.
+//
+
+import Foundation
+
+struct CatBreed {
+    let id: String
+    let name: String
+    let origin: String
+    let intelligence: Int
+    let imageUrl: String?
+}
